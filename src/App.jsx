0c4b214d8e2e562cb235831c5252d2a0ef3249a2
Musfiq708 <musfiqrahman708.com>
2025-08-05 19:50:36 +0600

    nav bar build

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import Banner from "./Components/Banner/Banner"
+import Navigation from "./Components/Nav/Navigation"
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Navigation></Navigation>
+      <Banner></Banner>
     </>
   )
 }
---
+++ src/Components/Nav/Navigation.jsx
@@ -0,0 +1,22 @@
+import logo from '../../assets/logo.png';
+import { TbCoinTakaFilled } from 'react-icons/tb';
+
+export default function Navigation() {
+  return (
+    <div className='flex items-center justify-between w-10/12 mx-auto pt-[50px]'>
+      <div>
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className='flex gap-12 items-center'>
+        <a className='nav-options' href="">Home</a>
+        <a className='nav-options' href="">Fixture</a>
+        <a className='nav-options' href="">Teams</a>
+        <a className='nav-options' href="">Schedules</a>
+        <div className='flex items-center border border-[#1313131A] rounded-xl py-4 px-5 gap-[10px] hover:border-red-600'>
+          <p className='text-base font-semibold '>0 Coin</p>
+          <TbCoinTakaFilled className='text-[35px] text-orange-400 font- border-amber-600' />
+        </div>
+      </div>
+    </div>
+  )
+}
